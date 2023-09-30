24fed473f60b98928ed52a671dd2fb34cc62eb29
Sudhanshu Pandey <sudhanshu.pandey@successive.com>
2023-09-30 14:51:42 +0530

    Android installation and backend added

---
+++ Constant.js
@@ -0,0 +1,4 @@
+export const MID = "YOUR_MID";
+export const API_URL = "http://localhost:8080/token.php";
+export const CALLBACK_URL = "https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID=";
+export const URL_SCHEME = "paytmMID+YOUR_MID";
---
+++ Paytm_Backend/keys.php
@@ -0,0 +1,4 @@
+<?php 
+$mid = "YOUR_MID";
+$merchant_Key = "YOUR_merchant_Key";
+?>
